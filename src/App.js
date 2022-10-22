b079d4df0d91471fe01106f0b922d4a148be537c
Kevin Christian Sianipar <36601304+kevincsianipar@users.noreply.github.com>
2022-10-23 00:36:42 +0700

    update removable list

--- src/App.js
+++ src/App.js
@@ -1,19 +1,57 @@
 import "antd/dist/antd.css";
+import './App.css';
 import React, { useState, useEffect } from 'react';
-import { Layout, Menu, Card, Row, Col, Select } from 'antd';
+import { Layout, Button, Card, Row, Col, Select } from 'antd';
+import {
+  CloseCircleTwoTone,
+} from '@ant-design/icons';
+import { Space } from 'antd';
 
 const App = () => {
   const { Header, Content, Footer } = Layout;
   const { Option } = Select;
-  const initialValue =  (localStorage.getItem('takenNumber') && JSON.parse(localStorage.getItem('takenNumber'))) || [];
-  const initialValueCategory =  (localStorage.getItem('winnerByCategory') && JSON.parse(localStorage.getItem('winnerByCategory'))) || [];
-  const initialCategory = 'hiburan1'
+  const initialValue = (localStorage.getItem('takenNumber') && JSON.parse(localStorage.getItem('takenNumber'))) || [];
+  const initialValueCategory = (localStorage.getItem('winnerByCategory') && JSON.parse(localStorage.getItem('winnerByCategory'))) || [];
+  const initialCategory = 'hiburan1';
+  const categoryOptions = [
+    {
+      text: "Hadiah Hiburan 1",
+      value: "hiburan1",
+      picks: 6
+    },
+    {
+      text: "Hadiah Hiburan 2",
+      value: "hiburan2",
+      picks: 6
+    },
+    {
+      text: "Hadiah Hiburan 3",
+      value: "hiburan3",
+      picks: 6
+    },
+    {
+      text: "Hadiah Utama 3",
+      value: "utama3",
+      picks: 3
+    },
+    {
+      text: "Hadiah Utama 2",
+      value: "utama2",
+      picks: 1
+    },
+    {
+      text: "Hadiah Utama 1",
+      value: "utama1",
+      picks: 1
+    }
+  ];
 
   const [num, setNum] = useState(0);
   const [selectedCategory, setSelectedCategory] = useState(initialCategory);
   const [selectedNumber, setSelectedNumber] = useState(initialValue);
   const [takenNumber, setTakenNumber] = useState(initialValue);
   const [winnerByCategory, setWinnerByCategory] = useState(initialValueCategory);
+  const [enableReset, setEnableReset] = useState(false);
 
   useEffect(() => {
     localStorage.setItem('takenNumber', JSON.stringify(takenNumber));
@@ -33,11 +71,13 @@ const App = () => {
       setSelectedNumber([rand, ...selectedNumber])
       setTakenNumber([rand, ...takenNumber])
       let appendedList = [];
-      if (winnerByCategory[selectedCategory]){
-        appendedList= winnerByCategory[selectedCategory]
+      if (winnerByCategory[selectedCategory]) {
+        appendedList = winnerByCategory[selectedCategory]
       }
-      let newObj = { ...winnerByCategory, [selectedCategory]: [rand, ...appendedList]};
-      setWinnerByCategory(newObj)
+      let newObj = { ...winnerByCategory, [selectedCategory]: [rand, ...appendedList] };
+      setTimeout(function () {
+        setWinnerByCategory(newObj)
+      }.bind(this), 6500);
     }
     return rand;
   }
@@ -47,13 +87,30 @@ const App = () => {
     return additionalZeros + String(value)
   }
 
-  const handleClick = () => {
+  const handleClickGetNumber = () => {
     setNum(getRandomWithManyExclusions(1, 20000, selectedNumber));
+    setTimeout(function () {
+      setEnableReset(true);
+    }.bind(this), 6500);
+  };
+
+
+  const handleClickReset = () => {
+    setNum(0);
+    setEnableReset(false);
   };
 
   function handleChange(value) {
     setNum(0);
     setSelectedCategory(value);
+  }
+
+  const handleRemoveNumber = value => {
+    let list = winnerByCategory[selectedCategory]
+    list = list.filter(item => item !== value)
+    console.log(value, list)
+    let newObj = { ...winnerByCategory, [selectedCategory]: list };
+    setWinnerByCategory(newObj)
   }
 
   return (
@@ -76,38 +133,90 @@ const App = () => {
       <Content className="site-layout" size='large' style={{ padding: '0 50px', marginTop: 100 }}>
         <div className="site-layout-background" style={{ padding: 24, height: 'calc(100vh - 100px)' }}>
           <Row>
-            <Col span={6}>
-              <h1>Kategori :
-                <Select defaultValue="hiburan1" style={{ width: 180, marginLeft: 12 }} onChange={handleChange}>
-                  <Option value="hiburan1">Hadiah Hiburan 1</Option>
-                  <Option value="hiburan2">Hadiah Hiburan 2</Option>
-                  <Option value="hiburan3">Hadiah Hiburan 3</Option>
-                  <Option value="hiburan4">Hadiah Hiburan 4</Option>
-                  <Option value="hiburan5">Hadiah Hiburan 5</Option>
-                  <Option value="hiburan6">Hadiah Hiburan 6</Option>
-                  <Option value="utama3">Hadiah Utama 3</Option>
-                  <Option value="utama2">Hadiah Utama 2</Option>
-                  <Option value="utama1">Hadiah Utama 1</Option>
-                </Select>
-              </h1>
+            <h1>Kategori :
+              <Select defaultValue="hiburan1" style={{ width: 250, marginLeft: 12, fontSize: 24 }} onChange={handleChange}>
+                {categoryOptions.map(cat => <Option value={cat.value}>{cat.text}</Option>)}
+              </Select>
+            </h1>
+          </Row>
+          <Row>
+            <h1>Jumlah Pengambilan : {categoryOptions.find(o => o.value === selectedCategory).picks}</h1>
+          </Row>
+          <Row type="flex" justify="center" align="top">
+            <h1 style={{ fontSize: 36 }}>Kupon Terpilih:</h1>
+          </Row>
+          {num > 0 && (
+            <div class="numbers">
+              <span class="numbers__window">
+                <span class="numbers__window__digit numbers__window__digit--0" data-fake="1234567890">0</span>
+              </span>
+              <span class="numbers__window">
+                <span class="numbers__window__digit numbers__window__digit--1" data-fake="2345678901">{renderCouponNumber(num).toString().charAt(1)}</span>
+              </span>
+              <span class="numbers__window">
+                <span class="numbers__window__digit numbers__window__digit--2" data-fake="3456789012">{renderCouponNumber(num).toString().charAt(2)}</span>
+              </span>
+              <span class="numbers__window">
+                <span class="numbers__window__digit numbers__window__digit--3" data-fake="5678901234">{renderCouponNumber(num).toString().charAt(3)}</span>
+              </span>
+              <span class="numbers__window">
+                <span class="numbers__window__digit numbers__window__digit--4" data-fake="6789012345">{renderCouponNumber(num).toString().charAt(4)}</span>
+              </span>
+              <span class="numbers__window">
+                <span class="numbers__window__digit numbers__window__digit--5" data-fake="7890123456">{renderCouponNumber(num).toString().charAt(5)}</span>
+              </span>
+            </div>
+          )}
+          {num == 0 && (
+            <div class="numbers">
+              <span class="numbers__window">
+                <span>0</span>
+              </span>
+              <span class="numbers__window">
+                <span>0</span>
+              </span>
+              <span class="numbers__window">
+                <span>0</span>
+              </span>
+              <span class="numbers__window">
+                <span>0</span>
+              </span>
+              <span class="numbers__window">
+                <span>0</span>
+              </span>
+              <span class="numbers__window">
+                <span>0</span>
+              </span>
+            </div>
+          )}
+          <Row gutter={16} type="flex" justify="center" align="top">
+            <Col>
+              <Button
+                onClick={handleClickGetNumber}
+                disabled={num > 0 ||
+                  winnerByCategory[selectedCategory] && 
+                  (winnerByCategory[selectedCategory]  > categoryOptions.find(o => o.value === selectedCategory).picks)}>
+                Ambil Nomor
+              </Button>
             </Col>
-            <Col span={8}>
-              <h1>Jumlah Pengambilan : </h1>
+            <Col>
+              <Button onClick={handleClickReset} type="danger" disabled={!enableReset}>Reset</Button>
             </Col>
           </Row>
-          
-          <h1 style={{fontSize: 36}}>Kupon Terpilih: {renderCouponNumber(num)}</h1>
-          <button onClick={handleClick}>Ambil Nomor</button>
+
           <Row>
-            {winnerByCategory[selectedCategory] && winnerByCategory[selectedCategory].map(value =>
-              <Col style={{display: 'flex', paddingBottom: 6, paddingTop: 6, paddingRight: 6, flexDirection: 'row'}}>
-                <Card style={{ width: 120, height: 80, padding: 0 }}>
-                  <h1 padding={0}>{renderCouponNumber(value)}</h1>
+            {winnerByCategory[selectedCategory] && (winnerByCategory[selectedCategory]).map(value =>
+              <Col style={{ display: 'flex', paddingTop: 24, paddingRight: 12, flexDirection: 'row' }}>
+                <Card style={{ width: 160, height: 100, padding: 0 }}>
+                  <div class="iconRemove" onClick={() => handleRemoveNumber(value)} style={{ position: "absolute", top: 0, right: 4 }}>
+                    <CloseCircleTwoTone twoToneColor="#eb2f96" />
+                  </div>
+                  <h1 padding={0} style={{ fontSize: 32 }}>{renderCouponNumber(value)}</h1>
                 </Card>
               </Col>
             )}
           </Row>
-          
+
         </div>
       </Content>
     </Layout>
