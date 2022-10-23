ab4ca68726419f61a0196aabc836f22a3ce03265
Kevin Christian Sianipar <36601304+kevincsianipar@users.noreply.github.com>
2022-10-23 14:59:13 +0700

    Updates

---
+++ precache-manifest.cb3c69b5c250087437d8db00c35e67f0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "c300abb0b8cbfb5242f3",
+    "url": "/doorprize-randomizer/static/css/main.4770df0f.chunk.css"
+  },
+  {
+    "revision": "c300abb0b8cbfb5242f3",
+    "url": "/doorprize-randomizer/static/js/main.c300abb0.chunk.js"
+  },
+  {
+    "revision": "f396410328fc823aed37",
+    "url": "/doorprize-randomizer/static/js/1.f3964103.chunk.js"
+  },
+  {
+    "revision": "0f7fc9e36c099f04358b",
+    "url": "/doorprize-randomizer/static/css/2.e91112bf.chunk.css"
+  },
+  {
+    "revision": "0f7fc9e36c099f04358b",
+    "url": "/doorprize-randomizer/static/js/2.0f7fc9e3.chunk.js"
+  },
+  {
+    "revision": "381b3940f97a3c606a4a",
+    "url": "/doorprize-randomizer/static/js/runtime~main.381b3940.js"
+  },
+  {
+    "revision": "2eb615bac047cd5e4993980c1dcf8c25",
+    "url": "/doorprize-randomizer/static/media/background.2eb615ba.png"
+  },
+  {
+    "revision": "dfcdb3a348a2b1314dc4c3c6873092fa",
+    "url": "/doorprize-randomizer/static/media/logohkbp.dfcdb3a3.png"
+  },
+  {
+    "revision": "ecdf7ad5a9a31dcbd3b87f386e677197",
+    "url": "/doorprize-randomizer/index.html"
+  }
+];
