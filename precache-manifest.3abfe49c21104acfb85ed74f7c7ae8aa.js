c338dfe0e552268134db16ee25a2983912b17599
Kevin Christian Sianipar <36601304+kevincsianipar@users.noreply.github.com>
2022-11-12 23:32:11 +0700

    Updates

---
+++ precache-manifest.3abfe49c21104acfb85ed74f7c7ae8aa.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "c3c9dde3b11468b3b8e4",
+    "url": "/doorprize-gotilon/static/css/main.60b8cf47.chunk.css"
+  },
+  {
+    "revision": "c3c9dde3b11468b3b8e4",
+    "url": "/doorprize-gotilon/static/js/main.c3c9dde3.chunk.js"
+  },
+  {
+    "revision": "f396410328fc823aed37",
+    "url": "/doorprize-gotilon/static/js/1.f3964103.chunk.js"
+  },
+  {
+    "revision": "63bd5f653d3668ea6c83",
+    "url": "/doorprize-gotilon/static/css/2.7fd32d7c.chunk.css"
+  },
+  {
+    "revision": "63bd5f653d3668ea6c83",
+    "url": "/doorprize-gotilon/static/js/2.63bd5f65.chunk.js"
+  },
+  {
+    "revision": "d4dbdc1f15cffb0a7173",
+    "url": "/doorprize-gotilon/static/js/runtime~main.d4dbdc1f.js"
+  },
+  {
+    "revision": "2eb615bac047cd5e4993980c1dcf8c25",
+    "url": "/doorprize-gotilon/static/media/background.2eb615ba.png"
+  },
+  {
+    "revision": "dfcdb3a348a2b1314dc4c3c6873092fa",
+    "url": "/doorprize-gotilon/static/media/logohkbp.dfcdb3a3.png"
+  },
+  {
+    "revision": "5ec6908ac96edd4d8bd34b7d990c8401",
+    "url": "/doorprize-gotilon/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/doorprize-randomizer/precache-manifest.fc9bdae43d1429ff745f2acc10171796.js"
+  "/doorprize-gotilon/precache-manifest.3abfe49c21104acfb85ed74f7c7ae8aa.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/doorprize-randomizer/index.html", {
+workbox.routing.registerNavigationRoute("/doorprize-gotilon/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
